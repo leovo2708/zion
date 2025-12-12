6c4d421b2f51cab06464933a118c5612da22b9c6
Leo Vo <leovo2708@gmail.com>
2025-12-12 13:54:12 +0700

    update bcm

--- bcm/index.js
+++ bcm/index.js
@@ -1,24 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("open").addEventListener("click", function () {
-    const songs = document
-      .getElementById("song")
-      .value.replaceAll(",", " ")
-      .split(" ");
-    for (const song of songs) {
-      const s = parseInt(song);
-      if (Number.isInteger(s)) {
-        const url =
-          "https://book.watv.org/newsong/newsong" +
-          (song[0] === "0" ? "-appx00" + s : s) +
-          "/";
-        const result = openUrl(url);
-        if (!result) return;
-      }
-    }
+const GS_SCRIPT_URL =
+  "https://script.google.com/macros/s/AKfycbyvHFutFktjUdVPFqtIC5ELZ14dgRzuoroY3DK-SuO6W5t1UUJK0udhhIeOJ6bcbbpe0g/exec";
+
+const dataPromise = fetch(GS_SCRIPT_URL, { method: "GET" })
+  .then((r) => {
+    if (!r.ok) throw new Error(r.status);
+    return r.json();
+  })
+  .catch((err) => {
+    console.error(err);
+    return null;
   });
+
+const domReady = new Promise((res) => {
+  if (document.readyState !== "loading") return res();
+  document.addEventListener("DOMContentLoaded", res, { once: true });
 });
 
-function openUrl(url) {
+const openUrl = (url) => {
   const win = window.open(url, "_blank");
   if (win) {
     //Browser has allowed it to be opened
@@ -30,4 +28,48 @@ function openUrl(url) {
   }
 
   return true;
-}
+};
+
+const openClick = () => {
+  const songIds = document
+    .getElementById("songs")
+    .value.replaceAll(" ", ",")
+    .replaceAll(";", ",")
+    .split(",")
+    .filter((item) => item !== "");
+  for (const id of songIds) {
+    var url;
+    if (id.startsWith("PL")) {
+      const s = parseInt(id.substring(2));
+      if (Number.isInteger(s)) {
+        url = "https://book.watv.org/newsong/newsong" + "-appx00" + s + "/";
+      }
+    } else {
+      const s = parseInt(id);
+      if (Number.isInteger(s)) {
+        url = "https://book.watv.org/newsong/newsong" + s + "/";
+      }
+    }
+
+    const result = openUrl(url);
+    if (!result) return;
+  }
+};
+
+Promise.all([dataPromise, domReady]).then(([data]) => {
+  document.getElementById("loadingOverlay").classList.add("hidden");
+  document.getElementById("open").addEventListener("click", openClick);
+
+  if (!data) return;
+
+  const select = document.getElementById("times");
+  data.forEach((row) => {
+    const opt = document.createElement("option");
+    opt.value = row.Songs;
+    opt.textContent = row.Date;
+    select.appendChild(opt);
+  });
+  select.addEventListener("change", (e) => {
+    document.getElementById("songs").value = e.target.value;
+  });
+});
