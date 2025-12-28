a8d72b8cfe2e118754eae5e19952e1b43cdfa6c0
Leo Vo <leovo2708@gmail.com>
2025-12-28 22:07:52 +0700

    update bcm

--- bcm/index.js
+++ bcm/index.js
@@ -1,20 +1,62 @@
 const GS_SCRIPT_URL =
   "https://script.google.com/macros/s/AKfycbyvHFutFktjUdVPFqtIC5ELZ14dgRzuoroY3DK-SuO6W5t1UUJK0udhhIeOJ6bcbbpe0g/exec";
 
-const dataPromise = fetch(GS_SCRIPT_URL, { method: "GET" })
-  .then((r) => {
-    if (!r.ok) throw new Error(r.status);
-    return r.json();
-  })
-  .catch((err) => {
-    console.error(err);
-    return null;
+async function fetchData() {
+  const r = await fetch(GS_SCRIPT_URL);
+  if (!r.ok) throw new Error(r.status);
+  return await r.json();
+}
+
+async function clearCache() {
+  return await fetch(GS_SCRIPT_URL, {
+    method: "POST",
+    mode: "no-cors",
+  });
+}
+
+async function domReady() {
+  if (document.readyState !== "loading") return;
+  await new Promise((resolve) =>
+    document.addEventListener("DOMContentLoaded", resolve, { once: true })
+  );
+}
+
+Promise.all([fetchData(), domReady()]).then(([data]) => {
+  document.getElementById("loadingOverlay").classList.add("hidden");
+  document.getElementById("open").addEventListener("click", openClick);
+  document.getElementById("times").addEventListener("change", (e) => {
+    document.getElementById("songs").value = e.target.value;
   });
 
-const domReady = new Promise((res) => {
-  if (document.readyState !== "loading") return res();
-  document.addEventListener("DOMContentLoaded", res, { once: true });
+  const urlParams = new URLSearchParams(window.location.search);
+  if (urlParams.has("admin")) {
+    document.getElementById("refresh").removeAttribute("hidden");
+    document.getElementById("refresh").addEventListener("click", refreshClick);
+  }
+
+  fillData(data);
 });
+
+const clearData = () => {
+  const select = document.getElementById("times");
+  select.selectedIndex = 0;
+  select.dispatchEvent(new Event("change"));
+  while (select.options.length > 1) {
+    select.remove(1);
+  }
+};
+
+const fillData = (data) => {
+  if (!data) return;
+
+  const select = document.getElementById("times");
+  data.forEach((row) => {
+    const opt = document.createElement("option");
+    opt.value = row.Songs;
+    opt.textContent = row.Date;
+    select.appendChild(opt);
+  });
+};
 
 const openUrl = (url) => {
   const win = window.open(url, "_blank");
@@ -56,20 +98,11 @@ const openClick = () => {
   }
 };
 
-Promise.all([dataPromise, domReady]).then(([data]) => {
+const refreshClick = async () => {
+  document.getElementById("loadingOverlay").classList.remove("hidden");
+  clearData();
+  await clearCache();
+  const data = await fetchData();
+  fillData(data);
   document.getElementById("loadingOverlay").classList.add("hidden");
-  document.getElementById("open").addEventListener("click", openClick);
-
-  if (!data) return;
-
-  const select = document.getElementById("times");
-  data.forEach((row) => {
-    const opt = document.createElement("option");
-    opt.value = row.Songs;
-    opt.textContent = row.Date;
-    select.appendChild(opt);
-  });
-  select.addEventListener("change", (e) => {
-    document.getElementById("songs").value = e.target.value;
-  });
-});
+};
