f6e69040e84ccbe9e83f90782a5e7f230ce61db9
Leo Vo <leovo2708@gmail.com>
2026-01-06 23:30:53 +0700

    update guide

--- bcm/index.js
+++ bcm/index.js
@@ -45,8 +45,13 @@ const updateClock = () => {
   const hh = String(now.getHours()).padStart(2, "0");
   const mm = String(now.getMinutes()).padStart(2, "0");
   const ss = String(now.getSeconds()).padStart(2, "0");
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
 
-  document.getElementById("clock").textContent = `${hh}:${mm}:${ss}`;
+  document.getElementById(
+    "clock"
+  ).textContent = `${hh}:${mm}:${ss} ngày ${day}/${month}/${year}`;
 };
 
 const clearData = () => {
