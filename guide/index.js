df96fe110e5adc1cae21b1d812252ef26b22feb3
Leo Vo <leovo2708@gmail.com>
2026-01-06 09:31:51 +0700

    update guide

--- guide/index.js
+++ guide/index.js
@@ -2,6 +2,40 @@ document.addEventListener("DOMContentLoaded", function (event) {
   let os = "";
   let so = "";
 
+  const images = document.querySelectorAll("img[data-size]");
+  images.forEach((img) => {
+    const size = img.getAttribute("data-size").trim();
+
+    images.forEach((img) => {
+      const size = img.dataset.size;
+
+      if (!size.endsWith("%")) return;
+
+      const scale = parseFloat(size) / 100;
+
+      const originalSrc = img.src;
+      const image = new Image();
+      image.crossOrigin = "anonymous"; // tránh lỗi CORS nếu có
+      image.src = originalSrc;
+
+      image.onload = () => {
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+
+        const newWidth = image.naturalWidth * scale;
+        const newHeight = image.naturalHeight * scale;
+
+        canvas.width = newWidth;
+        canvas.height = newHeight;
+
+        ctx.drawImage(image, 0, 0, newWidth, newHeight);
+
+        // đổi src của img thành ảnh đã resize
+        img.src = canvas.toDataURL("image/jpeg", 0.9);
+      };
+    });
+  });
+
   GLightbox({
     onOpen: () => {
       if (document.activeElement) {
